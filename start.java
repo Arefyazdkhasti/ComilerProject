46d88e3b4b5ed7e87552292a886515b70954ca67
Aref <44975938+Arefyazdkhasti@users.noreply.github.com>
2020-12-06 16:18:16 +0330

    printing tokens completed

--- start.java
+++ start.java
@@ -14,31 +14,46 @@ public class start {
         CharStream cs = null;
         try {
             cs = fromFileName("C:\\Users\\Acer\\IdeaProjects\\ComplierProject\\src\\input.txt");  //load the file
-
         } catch (Exception e) {
             System.err.println("No input file: " + e.getMessage());
             return;
         }
-
         myLexerLexer lexer = new myLexerLexer(cs);  //instantiate a lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer); //scan stream for tokens
-        myLexerParser parser = new myLexerParser(tokens);  //parse the tokens
-        ParseTree tree = parser.lexeme(); // parse the content and get the tree
-
         Token token = lexer.nextToken();
+        //CommonTokenStream tokens = new CommonTokenStream(lexer); //scan stream for tokens
+        //myLexerParser parser = new myLexerParser(tokens);  //parse the tokens
+        //ParseTree tree = parser.lexeme(); // parse the content and get the tree
+        //ParseTreeListener listener = new myLexerBaseListener();
+        //ParseTreeWalker walker = new ParseTreeWalker();
+        //walker.walk(listener, tree);
+        System.out.println("Tokens");
         while (token.getType() != Token.EOF) {
-            System.out.println(token.getText() + " " + token.getType());
+            String type=getTokenType(token.getType());
+            if(!type.equals("Default")) {
+                System.out.println(getTokenType(token.getType()) + " " + token.getText());
+            }else{
+                System.out.println(token.getText());
+            }
             token = lexer.nextToken();
         }
-        ParseTreeListener listener = new myLexerBaseListener();
-
-        ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(listener, tree);
     }
 
-    private TokenType getTokenType(TokenType tokenType){
+    private static String getTokenType (int tokenType){
         switch (tokenType){
-            case myLexerLexer.
+            case myLexerLexer.ID:
+                return "T_ID";
+            case myLexerLexer.STRING:
+                return "T_STRING";
+            case myLexerLexer.T_BOOLEAN:
+            case myLexerLexer.TRUE:
+            case myLexerLexer.FALSE:
+                return "T_BOOLEAN";
+            case myLexerLexer.CONST_NUMBER:
+                return "T_INT";
+            case myLexerLexer.Decimal_num:
+                return "T_DECIMAL";
+            default:
+                return "Default";
         }
     }
 }
